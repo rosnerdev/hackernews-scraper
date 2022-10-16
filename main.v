9ea7f2b31d731868ddda407b74129a87b4b99483
rosnerdev <yrosner123@gmail.com>
2022-10-16 12:01:36 +0300

    removed the 'main' function

--- main.v
+++ main.v
@@ -1,21 +1,19 @@
 import net.html
 import net.http
 
-fn main() {
-    content := http.get_text("https://news.ycombinator.com/")
+content := http.get_text("https://news.ycombinator.com/")
 
-	mut parsed_content := html.parse(content)
+mut parsed_content := html.parse(content)
 
-	articles := parsed_content.get_tag_by_attribute_value("class", "athing")
+articles := parsed_content.get_tag_by_attribute_value("class", "athing")
 
-	for article in articles {
-		for titleline in article.get_tags_by_attribute_value("class", "titleline") {
-			a_tag := titleline.get_tags("a")[0]
+for article in articles {
+	for titleline in article.get_tags_by_attribute_value("class", "titleline") {
+		a_tag := titleline.get_tags("a")[0]
 
-			title := a_tag.content
-			href := a_tag.attributes["href"]
+		title := a_tag.content
+		href := a_tag.attributes["href"]
 
-			println("\033[36m$title - \033[32m$href\n\033[31m")
-		}
+		println("\033[36m$title - \033[32m$href\n\033[31m")
 	}
 }
